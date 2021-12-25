05327492fecb13ea2ee724e4c371b23c5a68fd2f
Kds010 <rkfdktkeo2@naver.com>
2021-12-25 15:59:31 +0900

    package rename

---
+++ homeshopping/src/main/java/com/hs/hspt/HomeshoppingApplication.java
@@ -0,0 +1,13 @@
+package com.hs.hspt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeshoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeshoppingApplication.class, args);
+	}
+
+}
